991908c5dbbb4f41a782288b571830d694b3e034
amanarora9848 <aman.arora9848@gmail.com>
2023-04-17 23:36:39 +0200

    plan generator script improvements

--- generate_plan.sh
+++ generate_plan.sh
@@ -1,18 +1,35 @@
-plan="../enhsp-20.jar";
+plan="enhsp-20.jar";
 domain="base.pddl";
-problem="test.pddl";
 memory=5000;
 optimize=false;
 
+usage() {
+    echo "usage: generate_plan.sh -p <planner> -o <domain> -f <problem> -m <memory> -z"
+    echo "  -p <planner>    planner [path] to use - MANDATORY"
+    echo "  -o <domain>     domain [path] to use - optional, default is base.pddl"
+    echo "  -f <problem>    problem [path] to use - MANDATORY"
+    echo "  -m <memory>     specify java memory allowance, optional, default is 5000"
+    echo "  -z              uses optimizer if flag is passed, optional, default is false"
+}
+
+no_args="true";
 while getopts "p:o:f:m:z" flag; do
-    case "${flag}" in
+    case $flag in
         p) plan=${OPTARG} ;;
-        o) domain="${OPTARG}.pddl" ;;
-        f) problem="${OPTARG}" ;;
+        o) domain=${OPTARG} ;;
+        f) problem=${OPTARG} ;;
         m) memory=${OPTARG} ;;
         z) optimize='true' ;;
+        *) usage
+           exit;;
     esac
+    no_args="false";
 done
+
+[[ "$no_args" == "true" ]] && { usage; exit 1; }
+
+problem_name=( $(grep -Eo '[A-Za-z0-9]+' <<< "${problem}") )
+echo $problem_name
 
 config="Xmx${memory}m"
 
@@ -20,7 +37,7 @@ echo "planner: $plan";
 echo "problem: $problem";
 
 if $optimize; then
-    java -$config -jar $plan -o $domain -f "${problem}.pddl" -delta 0.5 -planner opt-blind > generated_plans/${problem}_with_optimizer.txt
+    java -$config -jar $plan -o $domain -f "${problem}" -delta 0.5 -planner opt-blind > generated_plans/${problem_name}_with_optimizer.txt
 else
-    java -$config -jar $plan -o $domain -f "${problem}.pddl" -delta 0.5 > generated_plans/"${problem}"_without_optimizer.txt
+    java -$config -jar $plan -o $domain -f "${problem}" -delta 0.5 > generated_plans/"${problem_name}"_without_optimizer.txt
 fi
