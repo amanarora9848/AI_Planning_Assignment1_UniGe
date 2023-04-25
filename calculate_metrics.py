42bc2dabb3eb488774f3d066351b27aeefb37337
amanarora9848 <aman.arora9848@gmail.com>
2023-04-25 17:53:56 +0200

    optimizer scripts, calculating metrics

---
+++ calculate_metrics.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import sys
+import pprint
+from math import sqrt
+import matplotlib.pyplot as plt
+
+plan_metrics = {
+    'Grounding Time': [],
+    '|F|': [],
+    '|X|': [],
+    '|A|': [],
+    '|P|': [],
+    '|E|': [],
+    'Metric (Search)': [],
+    'Planning Time (msec)': [],
+    'Heuristic Time (msec)': [],
+    'Search Time (msec)': [],
+    'Expanded Nodes': [],
+    'States Evaluated': [],
+    'Number of Dead-Ends detected': [],
+    'Number of Duplicates detected': []
+}
+
+mean_dict = {}
+mean_squared_error_dict = {}
+
+filename = sys.argv[1]
+plan_file = open(filename, 'r')
+
+observations = 0
+
+for line in plan_file:
+    for metric in plan_metrics.keys():
+        if line.startswith(metric):
+            val = float(line.split(':')[1])
+            plan_metrics[metric].append(float(line.split(':')[1]))
+    if "-------------------" in line:
+        observations += 1
+
+for metric in plan_metrics.keys():
+    # calculate the mean of every metric key
+    mean_dict[metric] = sum(plan_metrics[metric]) / len(plan_metrics[metric])
+    # Mean squared error
+    mean_squared_error_dict[metric] = 0
+    for data in plan_metrics[metric]:
+        mean_squared_error_dict[metric] += (data - mean_dict[metric]) ** 2
+    mean_squared_error_dict[metric] = sqrt(mean_squared_error_dict[metric] / len(plan_metrics[metric]))
+
+# Print the results
+print("Number of observations: ", observations)
+print("\n")
+print('Mean values: ')
+pprint.pprint(mean_dict)
+print("\n")
+print("Standard Deviation values: ")
+pprint.pprint(mean_squared_error_dict)
+
+# xm, ym = zip(*sorted(mean_dict.items()))
+# plt.plot(xm, ym)
+# plt.show()
+
+# xsq, ysq = zip(*sorted(mean_squared_error_dict.items()))
+# plt.plot(xsq, ysq)
+# plt.show()
--- generate_plan.sh
+++ generate_plan.sh
@@ -6,11 +6,12 @@ memory=5000;
 optimize=false;
 exec_all=false;
 planner="opt-blind"
+repeat=1;
 
 usage() {
     echo "Execute a single problem [or] all problem files in the current directory"
     echo "usage: generate_plan.sh -p <planner> -o <domain> -f <problem> -m <memory> -z"
-    echo " [or]: generate_plan.sh -p <planner> -o <domain> -az -m <memory>"
+    echo " [or]: generate_plan.sh -p <planner> -o <domain> -az -m <memory> -r <repeat> -c <planner>"
     echo
     echo "  -p <planner>    (mandatory) planner [path] to use"
     echo "  -o <domain>     (optional) domain [path] to use, default is base.pddl"
@@ -19,11 +20,12 @@ usage() {
     echo "  -c <planner>    (optional) specify planner configuration to use, default is opt-blind"
     echo "  -z              (optional) uses optimizer (opt-blind) if flag is passed, default is false"
     echo "  -a              (optional) executes all problems if flag is passed, default is false"
+    echo "  -r <repeat>     (optional) specify number of times to repeat the execution, default is 1"
     echo "  -h              display help"
 }
 
 no_args="true";
-while getopts "p:o:f:m:c:zah" flag; do
+while getopts "p:o:f:m:c:zar:h" flag; do
     case $flag in
         p) plan=${OPTARG} ;;
         o) domain=${OPTARG} ;;
@@ -32,6 +34,7 @@ while getopts "p:o:f:m:c:zah" flag; do
         c) planner=${OPTARG} ;;
         z) optimize='true' ;;
         a) exec_all='true' ;;
+        r) repeat=${OPTARG} ;;
         h) usage
            exit;;
         *) usage
@@ -48,15 +51,57 @@ configX="Xmx${memory}m"
 configS="Xms${memory}m"
 
 echo "planner: $plan";
+echo "planner configuration: $planner";
 
+# Function to execute the planner and write output to a file
 execute_plan() {
+    arg1=$1
+    # echo $arg1
     if $optimize; then
-        java -$configS -$configX -XX:+AlwaysPreTouch -jar $plan -o $domain -f "${problem}" -delta 0.5 -planner $planner > generated_plans/${problem_name}_with_optimizer_${planner}.txt
+        filename="generated_plans/${problem_name}_with_optimizer_${planner}.txt"
+        if [ $arg1 == true ]; then
+            echo "-------------------" >> ${filename}
+            java -$configS -$configX -XX:+AlwaysPreTouch -jar $plan -o $domain -f "${problem}" -delta 0.5 -planner $planner >> ${filename}
+        else
+            echo "-------------------" > ${filename}
+            java -$configS -$configX -XX:+AlwaysPreTouch -jar $plan -o $domain -f "${problem}" -delta 0.5 -planner $planner >> ${filename}
+        fi
     else
-        java -$configS -$configX -XX:+AlwaysPreTouch -jar $plan -o $domain -f "${problem}" -delta 0.5 > generated_plans/${problem_name}_without_optimizer.txt
+        java -$configS -$configX -XX:+AlwaysPreTouch -jar $plan -o $domain -f "${problem}" -delta 0.5 > ${problem_name}_without_optimizer.txt
     fi
 }
 
+#################### Progress Bar ####################
+bar_size=40
+bar_char_done="#"
+bar_char_todo="-"
+bar_percentage_scale=2
+# Thanks to https://www.baeldung.com/linux/command-line-progress-bar
+function show_progress {
+    current="$1"
+    total="$2"
+
+    # calculate the progress in percentage 
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+
+    # output the bar
+    echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
+
+    if [ $total -eq $current ]; then
+        echo -e "\nDONE"
+    fi
+}
+
+######################################################
+
+# Execute all problems in the current directory
 if $exec_all; then
     for i in problem*; do
         problem=$i
@@ -65,9 +110,20 @@ if $exec_all; then
         execute_plan
         sleep 2
     done
-    else
-        echo "problem: $problem";
-        problem_name=( $(grep -Eo '[A-Za-z0-9]+' <<< "${problem}") )
-        echo $problem_name
-        execute_plan
+# Execute a single problem
+else
+    echo "problem: $problem";
+    problem_name=( $(grep -Eo '[A-Za-z0-9]+' <<< "${problem}") )
+    execute_plan false
+    tot_prog=$repeat
+    (( repeat-=1 ))
+    echo "Number of repititions requested: $tot_prog";
+    for j in $(seq 1 $repeat); do
+        show_progress $j $tot_prog
+        execute_plan true
+    done
+    show_progress $tot_prog $tot_prog;
 fi
+
+# Calculate metrics
+./calculate_metrics.py $filename > generated_metrics/metrics_${problem_name}_${planner}.txt
